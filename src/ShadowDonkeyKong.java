0735ebf120f5dbfcb91edefefe4bed7fddb962f1
xinyueluo <xinyue.luo.1@student.unimelb.edu.au>
2025-04-13 00:32:28 +1000

    implement interface of homescreen of the game, setup basic logic of switching gamescreens

--- src/ShadowDonkeyKong.java
+++ src/ShadowDonkeyKong.java
@@ -1,4 +1,5 @@
 import bagel.*;
+
 import java.util.Properties;
 
 /**
@@ -14,15 +15,23 @@ public class ShadowDonkeyKong extends AbstractGame {
     private final Properties GAME_PROPS;
     private final Properties MESSAGE_PROPS;
 
+    // Switch between three different screens
+    private final String HOME_SCREEN = "HOMESCREEN";
+    private final String GAMEPLAY_SCREEN = "GAMEPLAYSCREEN";
+    private final String GAME_ENDING = "GAMEENDING";
+    private String gameScreen = HOME_SCREEN; // initialize the screen
+
+    // images used in this game
+    private final Image HomeScreenBG = new Image("res/background.png");
 
     public ShadowDonkeyKong(Properties gameProps, Properties messageProps) {
         super(Integer.parseInt(gameProps.getProperty("window.width")),
                 Integer.parseInt(gameProps.getProperty("window.height")),
                 messageProps.getProperty("home.title"));
-
         this.GAME_PROPS = gameProps;
         this.MESSAGE_PROPS = messageProps;
     }
+
 
 
     /**
@@ -33,6 +42,34 @@ public class ShadowDonkeyKong extends AbstractGame {
     protected void update(Input input) {
         if (input.wasPressed(Keys.ESCAPE)) {
             Window.close();
+        }
+        if (gameScreen.equals(HOME_SCREEN)){
+            HomeScreen(input);
+        } else if (gameScreen.equals(GAMEPLAY_SCREEN)) {
+            // add later
+        } else if (gameScreen.equals(GAME_ENDING)) {
+            // add later
+        }
+    }
+
+
+    private void HomeScreen (Input input){
+        HomeScreenBG.draw( Integer.parseInt(GAME_PROPS.getProperty("window.width")) / 2.0,
+                Integer.parseInt(GAME_PROPS.getProperty("window.height")) / 2.0);
+        final Font fontHomeTitle = new Font(GAME_PROPS.getProperty("font"),
+                Integer.parseInt(GAME_PROPS.getProperty("home.title.fontSize")));
+        fontHomeTitle.drawString(GAME_PROPS.getProperty("home.title"),
+                (Integer.parseInt(GAME_PROPS.getProperty("window.width"))-
+                        fontHomeTitle.getWidth(GAME_PROPS.getProperty("home.title"))) / 2,
+                Integer.parseInt(GAME_PROPS.getProperty("home.title.y")));
+        final Font fontHomePrompt = new Font(GAME_PROPS.getProperty("font"),
+                Integer.parseInt(GAME_PROPS.getProperty("home.prompt.fontSize")));
+        fontHomePrompt.drawString(GAME_PROPS.getProperty("home.prompt"),
+                (Integer.parseInt(GAME_PROPS.getProperty("window.width"))-
+                        fontHomePrompt.getWidth(GAME_PROPS.getProperty("home.prompt"))) / 2,
+                Integer.parseInt(GAME_PROPS.getProperty("home.prompt.y")));
+        if (input.wasPressed(Keys.ENTER)) {
+            gameScreen = GAMEPLAY_SCREEN;
         }
     }
 
