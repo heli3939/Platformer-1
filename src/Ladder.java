140098247f9758aea36d4c600c51648015a6021a
xinyueluo <xinyue.luo.1@student.unimelb.edu.au>
2025-04-16 21:32:19 +1000

    ladder seems to be work but can only reach 1 pixel lower than the platform now

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,16 +1,31 @@
 import bagel.Input;
 
-public class Ladder extends GameEntity{
+public class Ladder extends GameEntity {
     private final static String LADDER_IMG = "res/ladder.png";
 
     private double v_y = 0;
+
     public Ladder(int x, int y) {
         super(LADDER_IMG, x, y);
     }
 
     @Override
-    public void UpdatePostition(Platform[] platforms, Input input){
+    public void UpdatePostition(Platform[] platforms, Input input) {
+        int ladderHeight = (int) (this.getBoundingBox().bottom() -
+                this.getBoundingBox().top());
+        int platformHeight = (int) (platforms[0].getBoundingBox().bottom() -
+                platforms[0].getBoundingBox().top());
+        for (int i=0; i<platforms.length-1; i++) {
+            int higherBtm = (int) platforms[i+1].getBoundingBox().bottom();
+            int higherTop = (int) platforms[i+1].getBoundingBox().top();
 
+            if (this.isCollide(platforms[i])) {
+                y = higherBtm+ladderHeight / 2 - 29;
+                break;
+            }
+        }
     }
 
+
 }
+
